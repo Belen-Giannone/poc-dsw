c84e93d84805676fa02ea71adf3a9e96dd473683
Berenice D'Alleva <bdalleva@frro.utn.edu.ar>
2025-08-11 21:24:21 -0300

    implementacion

---
+++ datos/tareas-datos.js
@@ -0,0 +1,7 @@
+const tareas = [
+    { id: 1, materia: "Matematica", fechaentrega: "25/03/2025", numerotarea: 1 },
+  { id: 2, materia: "Lengua", fechaentrega: "28/06/2025", numerotarea: 5 },
+  { id: 3, materia: "Historia", fechaentrega: "15/10/2025", numerotarea: 8 },
+  ];
+
+module.exports = tareas;
